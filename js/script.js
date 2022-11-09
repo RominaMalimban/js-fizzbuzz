17ac3fe3a01bbc7041902d76c005ac5f97e9ce90
RominaMalimban <romina.lyn@hotmail.it>
2022-11-09 13:36:49 +0100

    commenti con procedimento da eseguire

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+
+// devo creare un ciclo for per numeri da 1 a 50
+
+
+    // creo le condizioni:
+        // se divisibile per 3 deve comparire Fizz
+        // se divisibile per 5 deve comparire Buzz
+        // se divisibile per 3 e per 5 deve comparire FizzBuz
